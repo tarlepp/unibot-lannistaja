9d162f01192831b33381f5e4802e05d5f69ec67e
tarlepp <tarmo.leppanen@kase.fi>
2014-12-27 16:15:10 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-unibot-lannistaja
-=================
+#unibot-lannistaja
+Simple "Lannistajakuha" plugin for UniBot. Basically this will fetch "slogan" of today from http://lannistajakuha.com/
+website.
 
-Simple "Lannistajakuha" plugin for UniBot
+## Install
+To your UniBot application
+```npm install git://github.com/tarlepp/unibot-lannistaja --save```
+
+And after that register new plugin on IRC channels what you need
+```plugin [#channel] lannistaja```
+
+ps. remember to restart your UnitBot.
---
+++ index.js
@@ -0,0 +1,38 @@
+/**
+ * Plugin dependencies.
+ *
+ * @type {exports}
+ */
+var cheerio = require('cheerio');
+var helpers = require('unibot-helpers');
+
+/**
+ * Weather plugin for UniBot.
+ *
+ * This plugin fetches weather data from http://api.openweathermap.org API.
+ *
+ * @param  {Object} options Plugin options object, description below.
+ *   db: {mongoose} the mongodb connection
+ *   bot: {irc} the irc bot
+ *   web: {connect} a connect + connect-rest webserver
+ *   config: {object} UniBot configuration
+ *
+ * @return  {Function}  Init function to access shared resources
+ */
+module.exports = function init(options) {
+    // Actual plugin implementation.
+    return function plugin(channel) {
+        return {
+            "^!lannistus$": function lannistus(from, matches) {
+                helpers.download('http://lannistajakuha.com/', function callback(data) {
+                    var $ = cheerio.load(data);
+                    var lannistus = $('div.lannistus p').text().trim();
+
+                    if (lannistus) {
+                        channel.say(lannistus);
+                    }
+                });
+            }
+        };
+    };
+};
